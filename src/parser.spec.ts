1c004602b80e0ad0d1b48543d3f014bccf95a4a6
Preston Neal <mrprestonneal@gmail.com>
2023-08-25 19:07:28 -0700

    parse object to sql

---
+++ src/parser.spec.ts
@@ -0,0 +1,17 @@
+import { parser } from './parser'
+
+describe('parser', () => {
+    it('should produce sql query from json', () => {
+        const testObj = {
+            topKey: {
+                nestedKey1: 1234,
+                nestedKey2: 'abcd'
+            }
+        }
+
+        const json = JSON.stringify(testObj)
+        const expectedResult = `SELECT JSON_VALUE(json_blob.topKey.nestedKey1), JSON_VALUE(json_blob.topKey.nestedKey2) FROM <project>.<datastream>.<dataset>`
+        const output = parser.jsonToSqlView(json)
+        expect(output).toEqual(expectedResult) 
+    })
+})
---
+++ src/parser.ts
@@ -0,0 +1,20 @@
+function jsonToSqlView(json: string) {
+    const parsed = JSON.parse(json)
+    const view = Object.keys(parsed)[0]
+    const columns = Object.keys(parsed[view])    
+    let sql = `SELECT JSON_VALUE(` 
+    for(let i=0; i<columns.length; i++){
+        const col = columns[i]
+        if (i+1 < columns.length) {
+            sql = `${sql}json_blob.${view}.${col}), JSON_VALUE(` 
+        } else {
+            sql = `${sql}json_blob.${view}.${col})`
+        }
+    }
+    sql = `${sql} FROM <project>.<datastream>.<dataset>`
+    return sql
+}
+
+export const parser = {
+    jsonToSqlView
+}
