0f88d4097fb09f4aad92b00d8e3e78a191c35961
Preston Neal <mrprestonneal@gmail.com>
2023-08-25 19:23:38 -0700

    detecting ints and strings

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -1,17 +1,32 @@
-import { jsonToSqlView } from './parser'
+import { jsonToSqlView, datatype } from './parser'
 
 describe('parser', () => {
-    it('should produce sql query from json', () => {
-        const testObj = {
-            topKey: {
-                nestedKey1: 1234,
-                nestedKey2: 'abcd'
+    describe('jsonToSqlView', () => {
+        it('should produce sql query from json', () => {
+            const testObj = {
+                topKey: {
+                    nestedKey1: 1234,
+                    nestedKey2: 'abcd'
+                }
             }
-        }
 
-        const json = JSON.stringify(testObj)
-        const expectedResult = `SELECT JSON_VALUE(json_blob.topKey.nestedKey1), JSON_VALUE(json_blob.topKey.nestedKey2) FROM <project>.<datastream>.<dataset>`
-        const output = jsonToSqlView(json)
-        expect(output).toEqual(expectedResult) 
+            const json = JSON.stringify(testObj)
+            const expectedResult = `SELECT CAST(JSON_VALUE(json_blob.topKey.nestedKey1) as INTEGER), CAST(JSON_VALUE(json_blob.topKey.nestedKey2) as STRING) FROM <project>.<datastream>.<dataset>`
+            const output = jsonToSqlView(json)
+            expect(output).toEqual(expectedResult) 
+        })
+    })
+
+    describe('datatype', () => {
+        it('should return STRING for string', () => {
+            const input = 'abcd'
+            const result = datatype(input)
+            expect(result).toEqual("STRING")
+        })
+        it('should return INTEGER for integer', () => {
+            const input = 1234
+            const result = datatype(input)
+            expect(result).toEqual("INTEGER")
+        })
     })
 })
--- src/parser.ts
+++ src/parser.ts
@@ -1,14 +1,26 @@
+export function datatype(input: any) {
+    if (typeof input === 'string') {
+        return "STRING"
+    }
+    if (typeof input === 'number' && Number.isInteger(input)) {
+        return "INTEGER"
+    }
+}
+
 export function jsonToSqlView(json: string) {
     const parsed = JSON.parse(json)
     const view = Object.keys(parsed)[0]
     const columns = Object.keys(parsed[view])    
-    let sql = `SELECT JSON_VALUE(` 
+    const values = Object.values(parsed[view])
+
+    let sql = `SELECT CAST(JSON_VALUE(` 
     for(let i=0; i<columns.length; i++){
         const col = columns[i]
+        const type = datatype(values[i])
         if (i+1 < columns.length) {
-            sql = `${sql}json_blob.${view}.${col}), JSON_VALUE(` 
+            sql = `${sql}json_blob.${view}.${col}) as ${type}), CAST(JSON_VALUE(` 
         } else {
-            sql = `${sql}json_blob.${view}.${col})`
+            sql = `${sql}json_blob.${view}.${col}) as ${type})`
         }
     }
     sql = `${sql} FROM <project>.<datastream>.<dataset>`
